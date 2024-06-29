06b276618127a29b402ee334805e7fa6c3381e50
zisooya <zisooya319@gmail.com>
2024-06-29 23:21:24 +0900

    :sparkles: feat(common): 기본 구조 생성

---
+++ src/main/java/com/jg/ebook/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.jg.ebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String mainPage(Model model){
+        return "ebook/ebook";
+    }
+}
