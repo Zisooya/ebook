60b0f140ed89906401cf092e87c00d38f9ac2b71
zisooya <zisooya319@gmail.com>
2024-06-29 22:04:24 +0900

    :tada: Initial commit

---
+++ src/test/java/com/jg/ebook/EbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
